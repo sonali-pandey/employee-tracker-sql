1443d973d63067d09e8df640e60011bf7c47aa38
Sona <sonali23.1293@gmail.com>
2021-09-11 19:01:17 -0400

    view all roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,14 @@ VALUE
 ('Engineering'),
 ('IT'),
 ('Customer service');
+
+INSERT INTO roles (title, salary, department_id)
+VALUE
+('Sales Lead', '100000', '1'),
+('Salesperson', '80000', '1'),
+('Accountant', '125000', '2'),
+('Lead Engineer', '150000', '4'),
+('Software Engineer', '120000', '4'),
+('Lawyer', '190000', '3'),
+('Legal Team Lead', '250000', '3'),
+('System Engineer', '119000', '5');
